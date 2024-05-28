fd265018c78c147dca0561f1a0d16ea6759d61ed
thai-ru <robinthairu34@gmail.com>
2024-05-28 21:44:50 +0300

    data access object

---
+++ app/src/main/java/com/thairu/hrnow/data/employeeDAO.kt
@@ -0,0 +1,26 @@
+package com.thairu.hrnow.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface EmployeeDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addEmployee(employee: Employee)
+
+    @Query("SELECT * FROM employees WHERE employeeId = :empId")
+    fun findEmployeeById(empId: String) : Employee
+
+    @Query("SELECT * FROM employees")
+    fun getAllEmployees(): List<Employee>
+
+    @Update
+    suspend fun updateEmployeeDetails(employee: Employee)
+
+    @Delete
+    suspend fun deleteEmployee(employee: Employee)
+}
